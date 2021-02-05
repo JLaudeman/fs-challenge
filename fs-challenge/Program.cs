9b3fa971585a6673d6f585ded2fa351a9e8966bf
JLaudeman <jacob@laudeman.com>
2021-02-05 11:54:38 -0500

    Fix several issues

--- fs-challenge/Program.cs
+++ fs-challenge/Program.cs
@@ -22,12 +22,9 @@ namespace fschallenge
     {
         public static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Error: missing path to database");
-                return;
-            }
-            string connectionString = string.Format("URI=file:{0};", args[0]);
+            Console.WriteLine("Enter the path to .db3");
+            string dbPath = Console.ReadLine();
+            string connectionString = string.Format("URI=file:{0};", dbPath);
 
             int filterSize = 3;
             Console.WriteLine("Type a new filter size, or press enter to use the default");
@@ -54,35 +51,58 @@ namespace fschallenge
             SqliteConnection conn = new SqliteConnection(connectionString);
             conn.Open();
             SqliteCommand rcomm = conn.CreateCommand();
-            const string sql = "SELECT test_uid FROM Tests";
+            const string sql = "SELECT test_uid, sTime, PlaneID, Operator FROM Tests";
             rcomm.CommandText = sql;
             SqliteDataReader reader = rcomm.ExecuteReader();
 
-            StringBuilder csvResult = new StringBuilder();
-            csvResult.AppendLine("Average Roughness\tRMS Roughness\tMinimum Height\tMinimum Height Location\tMaximum Height\tMaximum Height Location\tHeight Range\tNo. of Included Measurements\tNo. of Excluded Measurements");
+            StringBuilder unfilteredResult = new StringBuilder();
+            unfilteredResult.AppendLine("Test ID,Time,Plane ID,Operator,Average Roughness,RMS Roughness,Minimum Height,Minimum Height Location," +
+            	                        "Maximum Height,Maximum Height Location,Height Range");
+            StringBuilder filteredResult = new StringBuilder();
+            filteredResult.AppendLine("Test ID,Time,Plane ID,Operator,Average Roughness,RMS Roughness,Minimum Height,Minimum Height Location," +
+            	                      "Maximum Height,Maximum Height Location,Height Range,No. of Filtered Measurements,No. of Unfiltered Measurements");
 
             while (reader.Read())
             {
-                int testID = reader.GetInt32(0);
+                long testID = reader.GetInt64(0);
                 List<Measurement> testMeasurements = QueryMeasurements(testID, conn);
 
                 if (testMeasurements == null)
                 {
-                    csvResult.AppendLine(string.Format("Invalid Test (ID: {0})", testID));
+                    unfilteredResult.AppendLine(string.Format("Invalid Test (ID: {0})", testID));
+                    filteredResult.AppendLine(string.Format("Inavlaid Test (ID: {0})", testID));
                     continue;
                 }
 
-                csvResult.AppendLine(SummarizeMeasurements(testMeasurements, filterSize));
+                string operatorString;
+                try
+                {
+                    operatorString = reader.GetString(3);
+                }
+                catch
+                {
+                    operatorString = "N/A";
+                }
+
+                string testInfo = string.Format("{0},{1},{2},{3},", testID, reader.GetDateTime(1), reader.GetString(2), operatorString);
+                unfilteredResult.Append(testInfo);
+                filteredResult.Append(testInfo);
+
+                unfilteredResult.AppendLine(UnfilteredTestSummary(testMeasurements));
+                filteredResult.AppendLine(FilteredTestSummary(testMeasurements, filterSize));
             }
 
-            File.WriteAllText("test_summaries.csv", csvResult.ToString());
+            File.WriteAllText("unfiltered_test_summaries.csv", unfilteredResult.ToString());
+            File.WriteAllText("filtered_test_summaries.csv", filteredResult.ToString());
 
             reader.Close();
             rcomm.Dispose();
             conn.Dispose();
+
+            Console.ReadLine();
         }
 
-        static List<Measurement> QueryMeasurements(int testID, SqliteConnection conn)
+        static List<Measurement> QueryMeasurements(long testID, SqliteConnection conn)
         {
             if (IsValidTest(testID, conn) != true)
             {
@@ -107,7 +127,7 @@ namespace fschallenge
             return measurements;
         }
 
-        static bool IsValidTest(int testID, SqliteConnection conn)
+        static bool IsValidTest(long testID, SqliteConnection conn)
         {
             string validTestSQL = string.Format("SELECT Count(measurement_uid) FROM Measurements WHERE test_uid = {0}", testID);
             SqliteCommand isValidComm = conn.CreateCommand();
@@ -120,7 +140,7 @@ namespace fschallenge
             return (measurementCount == 1000);
         }
 
-        static string SummarizeMeasurements(List<Measurement> measurements, int filterSize)
+        static string UnfilteredTestSummary(List<Measurement> measurements)
         {
             double mu = 0;
             double minHeight = measurements[0].height;
@@ -135,22 +155,62 @@ namespace fschallenge
             mu = mu / 1000;
 
             double R_q = 0;
+            double R_a = 0;
             foreach (Measurement m in measurements)
             {
-                R_q += (m.height - mu) * (m.height - mu);
+                R_q += Math.Pow(m.height - mu, 2);
+                R_a += Math.Abs(m.height - mu);
+
+                if (m.height > maxHeight)
+                {
+                    maxHeight = m.height;
+                    maxHeightLocation = (m.x, m.y);
+                }
+                else if (m.height < minHeight)
+                {
+                    minHeight = m.height;
+                    minHeightLocation = (m.x, m.y);
+                }
             }
             R_q = Math.Sqrt(R_q / 1000);
+            R_a = R_a / 1000;
+
+            return string.Format("{0},{1},{2},\"{3}\",{4},\"{5}\",{6}",
+                R_a, R_q, minHeight, minHeightLocation, maxHeight, maxHeightLocation, maxHeight - minHeight);
+        }
+
+        static string FilteredTestSummary(List<Measurement> measurements, int filterSize)
+        {
+            double mu = 0;
+            double minHeight = measurements[0].height;
+            double maxHeight = measurements[0].height;
+            (double, double) minHeightLocation = (measurements[0].x, measurements[0].y);
+            (double, double) maxHeightLocation = (measurements[0].x, measurements[0].y);
+
+            foreach (Measurement m in measurements)
+            {
+                mu += m.height;
+            }
+            mu = mu / 1000;
+
+            double stdDeviation = 0;
+            foreach (Measurement m in measurements)
+            {
+                stdDeviation += (m.height - mu) * (m.height - mu);
+            }
+            stdDeviation = Math.Sqrt(stdDeviation / 1000);
 
             double R_a = 0;
+            double R_q = 0;
             int numFiltered = 0;
             foreach (Measurement m in measurements)
             {
-                if (Math.Abs(m.height - mu) > (filterSize * R_q))
+                if (Math.Abs(m.height - mu) > (filterSize * stdDeviation))
                 {
                     numFiltered++;
                     continue;
                 }
-
+                R_q += Math.Pow(m.height - mu, 2);
                 R_a += Math.Abs(m.height - mu);
 
                 if (m.height > maxHeight)
@@ -165,9 +225,10 @@ namespace fschallenge
                 }
             }
             R_a = R_a / (1000 - numFiltered);
+            R_q = Math.Sqrt(R_q / (1000 - numFiltered));
 
-            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}",
-                R_a, R_q, minHeight, minHeightLocation, maxHeight, maxHeightLocation, maxHeight - minHeight, 1000 - numFiltered, numFiltered);
+            return string.Format("{0},{1},{2},\"{3}\",{4},\"{5}\",{6},{7},{8}",
+                R_a, R_q, minHeight, minHeightLocation, maxHeight, maxHeightLocation, maxHeight - minHeight, numFiltered, 1000 - numFiltered);
         }
     }
 }
